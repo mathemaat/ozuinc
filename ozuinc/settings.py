bdab573512507768330b33a98b22fd3570e4eafd
Marc Schwartz <mathemaat@xs4all.nl>
2018-07-11 12:31:16 +0200

    Optimise path definitions in settings.py

--- ozuinc/settings.py
+++ ozuinc/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
-import os
+from pathlib import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_PACKAGE = Path(__file__).resolve().parent
+BASE_DIR = PROJECT_PACKAGE.parent
 
 
 # Quick-start development settings - unsuitable for production
